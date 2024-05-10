6d690e7a756666d6885c32fa62922ddb28b8ba7c
Wonjin Kim <58078984+wjcldply@users.noreply.github.com>
2024-05-10 21:34:29 +0900

    Add files via upload
    added similarity.py
    -> calculates cosine similarity between given strings

---
+++ similarity.py
@@ -0,0 +1,26 @@
+import torch
+from transformers import BertModel, BertTokenizer
+# Setting Device
+if torch.backends.mps.is_built(): device = torch.device("mps")
+elif torch.cuda.is_available(): device = torch.device("cuda")
+else: device = torch.device("cpu")
+
+
+def similarity(sentences):
+    bertVersion = 'bert-large-uncased'
+    tokenizer = BertTokenizer.from_pretrained(bertVersion)
+    model = BertModel.from_pretrained(bertVersion)
+
+    encodings = tokenizer(sentences, return_tensors='pt', padding=True)
+    encodings = encodings.to(device)
+
+    # Create Embeddings
+    model = model.to(device)
+    with torch.no_grad():
+        embeddings = model(**encodings)
+
+    embeddings_avg = embeddings[0].mean(axis=2)
+
+    return torch.cosine_similarity(embeddings_avg[0].reshape(1,-1), embeddings_avg[1].reshape(1,-1)).item()
+
+print(similarity(['this is test sentence 1', 'random']))
