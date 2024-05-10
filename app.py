d75b6ca1a480102bfd87ddc1eb14b222c353633a
Wonjin Kim <58078984+wjcldply@users.noreply.github.com>
2024-05-11 01:27:01 +0900

    Update app.py
    버트 모델 미리 로드하도록 수정

--- app.py
+++ app.py
@@ -5,29 +5,42 @@ from similarity import similarity
 from rouge import rouge_score
 from roberta import get_roberta_similarity
 
+### Bert Model Loading
+import torch
+from transformers import BertModel, BertTokenizer
+# Setting Device
+if torch.backends.mps.is_built(): device = torch.device("mps")
+elif torch.cuda.is_available(): device = torch.device("cuda")
+else: device = torch.device("cpu")
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased')
+model = BertModel.from_pretrained('bert-large-uncased')
+###
+
 app = Flask(__name__)
 api = Api(app)
+
 
 @api.route('/sentence_length')
 class SentenceLength(Resource):
     def post(self):
         # POST 요청에서 문장 두 개를 받음
-        sentence1 = request.form.get('sentence1', '')  
+        sentence1 = request.form.get('sentence1', '')
         sentence2 = request.form.get('sentence2', '')
-        
-        sim = similarity([sentence1,sentence2])    
-        rouge = rouge_score([sentence1],[sentence2])
-        roberta = get_roberta_similarity(sentence1,sentence2)
+
+        sim = similarity([sentence1, sentence2], model, tokenizer)
+        rouge = rouge_score([sentence1], [sentence2])
+        # roberta = get_roberta_similarity(sentence1, sentence2)
         len1 = len(sentence1)
         len2 = len(sentence2)
 
         return {
-            'similarity' : sim,
-            'rouge' : rouge,
-            'roberta' : roberta,
-            'len1' : len1,
-            'len2' : len2
+            'similarity': sim,
+            'rouge': rouge,
+            # 'roberta': roberta,
+            'len1': len1,
+            'len2': len2
         }
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
